4078073ece963bf9dd8f7c7669aef5c62b50e581
rodrigocmo <47004786+rodrigocmo@users.noreply.github.com>
2023-12-18 19:42:26 -0300

    Start with functional interface

---
+++ src/main/java/com/example/functinterface/FunctInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.functinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/functinterface/FunctInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.functinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
